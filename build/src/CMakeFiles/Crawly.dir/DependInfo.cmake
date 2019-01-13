97c2b2233bb53b5526767e23a1a126a813e9d0c7
Varun C H <varunc_h@hotmail.com>
2019-01-13 22:24:42 +0530

    Adding CMake support.

---
+++ build/src/CMakeFiles/Crawly.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/wind2000/Projects/github/crawly/src/cJSON.c" "/home/wind2000/Projects/github/crawly/build/src/CMakeFiles/Crawly.dir/cJSON.c.o"
+  "/home/wind2000/Projects/github/crawly/src/main.c" "/home/wind2000/Projects/github/crawly/build/src/CMakeFiles/Crawly.dir/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
